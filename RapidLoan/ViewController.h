e7f9b9fc7d90c9fcb5beef644721ca2423d54d1f
admin <admin@admindeimac.local>
2018-01-22 09:59:34 +0800

    Initial Commit

---
+++ RapidLoan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RapidLoan
+//
+//  Created by admin on 2018/1/22.
+//  Copyright © 2018年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RapidLoanTests/RapidLoanTests.m
@@ -0,0 +1,39 @@
+//
+//  RapidLoanTests.m
+//  RapidLoanTests
+//
+//  Created by admin on 2018/1/22.
+//  Copyright © 2018年 admin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RapidLoanTests : XCTestCase
+
+@end
+
+@implementation RapidLoanTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
